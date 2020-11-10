f71f3abadad6059b2084666503bd7dbd45c21113
SAKDIPAT ONTOUM <61130500229@st.sit.kmutt.ac.th>
2020-11-10 09:55:32 +0700

    [ADD] the image picker

--- app/src/main/java/com/ailoveyou/trashrecognition/views/ImageClassifierActivity.kt
+++ app/src/main/java/com/ailoveyou/trashrecognition/views/ImageClassifierActivity.kt
@@ -1,53 +1,79 @@
 package com.ailoveyou.trashrecognition.views
 
 
-import android.graphics.drawable.BitmapDrawable
+import android.os.Build
 import android.os.Bundle
-import android.view.View
-import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.ailoveyou.trashrecognition.R
-import com.ailoveyou.trashrecognition.tflite.Classifier
-import java.text.NumberFormat;
+import kotlinx.android.synthetic.main.activity_image_classifier.*
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
 
 
-class ImageClassifierActivity : AppCompatActivity(), View.OnClickListener {
+class ImageClassifierActivity : AppCompatActivity(){
 
-    private val mInputSize = 224
-    private val mModelPath = "model_unquant.tflite"
-    private val mLabelPath = "labels.txt"
-    private lateinit var classifier: Classifier
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_image_classifier)
-        initClassifier()
-        initViews()
+        img_pick_btn.setOnClickListener {
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) ==
+                    PackageManager.PERMISSION_DENIED
+                ) {
+                    val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE);
+                    requestPermissions(permissions, PERMISSION_CODE);
+                } else {
+                    pickImageFromGallary();
+                }
+            }
+            else{
+                pickImageFromGallary();
+            }
+        }
     }
 
-    private fun initClassifier() {
-        classifier = Classifier(assets, mModelPath, mLabelPath, mInputSize)
+    private  fun pickImageFromGallary() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_PICK_CODE)
     }
 
-    private fun initViews() {
-        findViewById<ImageView>(R.id.iv_1).setOnClickListener(this)
-
+    companion object{
+        private val IMAGE_PICK_CODE = 1000;
+        private val PERMISSION_CODE = 1001;
     }
 
-    override fun onClick(view: View?) {
-        val defaultFormat = NumberFormat.getPercentInstance()
-        defaultFormat.minimumFractionDigits = 1;
-        val bitmap = ((view as ImageView).drawable as BitmapDrawable).bitmap
-        val result = classifier.recognizeImage(bitmap)
-        runOnUiThread { Toast.makeText(
-            this,
-            result?.get(0)?.title +" "
-                    + String.format("%.2f",(result?.get(0)?.confidence?.times(100)))+"%",
-            Toast.LENGTH_SHORT
-        ).show()
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            PERMISSION_CODE -> {
+                if (grantResults.size > 0 && grantResults[0] ==
+                    PackageManager.PERMISSION_GRANTED
+                ) {
+                    //permission from popup granted
+                    pickImageFromGallary()
+                } else {
+                    //permission from popup denied
+                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
 
-
+    //handle result of picked image
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE){
+            iv_1.setImageURI(data?.data)
         }
     }
 }
+
